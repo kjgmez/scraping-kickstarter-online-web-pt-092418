807546ca3ce5033f75d7d00f852f5ffcfb52c894
Kevin J Gomez <kjgmez1@gmail.com>
2018-11-25 17:11:57 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,3 +13,5 @@ end
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("")
+
+create_project_hash
